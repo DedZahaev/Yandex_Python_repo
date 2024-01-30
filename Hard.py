b3a6d58b788b8bf5335dd4ce2dba3290b745773a
Danila Marychev <marychtv5@gmail.com>
2024-01-30 21:56:43 +0300

    Программа, которая преобразует введённую строку из горизонтальной записи в вертикальную.

--- Hard.py
+++ Hard.py
@@ -1,8 +1,6 @@
-N = int(input()) 
-flag = "YES"
-for i in range(N):
-    b = input()
-    if b[0] != "а" and b[0] != "б" and b[0] != "в":
-        flag = "NO"
-print(flag)
+N = input()
+for letter in N:
+    print(letter, end="\n")
+
+
  
