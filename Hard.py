ebbabdb7941ca9f550985bd4189dac4b4761b3a3
Danila Marychev <marychtv5@gmail.com>
2024-01-30 22:48:37 +0300

    Программа, которая сокращает длинные заголовки до требуемой длины и завершает их многоточием ... при необходимости.

--- Hard.py
+++ Hard.py
@@ -1,6 +1,14 @@
-N = input()
-for letter in N:
-    print(letter, end="\n")
-
+L = int(input())
+N = int(input())
+a = []
+for i in range(N):
+    a.append(input())  
+for item in a:
+    lenth = len(item)
+    if lenth > L:
+        print(item[:L - 3] + "...")
+    else:
+        print(item)
+    
 
  
