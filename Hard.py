ce56d3c0191889f05b7d531c77490422c440e01c
Danila Marychev <marychtv5@gmail.com>
2024-01-30 21:19:15 +0300

    Программа, которая проверяет, что первая буква во всех словах — А, Б или В.

--- Hard.py
+++ Hard.py
@@ -1,24 +1,8 @@
-n = int(input())
-
-count = 0
-max_numbers = 0
-mazday = 0
-
-for i in range(2, 11):
-    conclusion1 = n
-    count = 0 
-    while True:
-        conclusion2 = conclusion1 % i
-        conclusion1 = conclusion1 // i
-         
-        if conclusion1 == 0: 
-            count += conclusion2
-            if max_numbers < count:
-                max_numbers = count
-                mazday = i
-            elif mazday != 0 and max_numbers == count and mazday > i:
-                mazday = i
-            break
-        else:
-            count += conclusion2
-print(mazday)
+N = int(input()) 
+flag = "YES"
+for i in range(N):
+    b = input()
+    if b[0] != "а" and b[0] != "б" and b[0] != "в":
+        flag = "NO"
+print(flag)
+ 
