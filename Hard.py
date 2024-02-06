c76d3d162795fbbe19cfd44e9a9ce38691025f92
Danila Marychev <marychtv5@gmail.com>
2024-02-06 21:37:10 +0300

    Сложение 2 чисел разделенных пробелом + Cleanup 2 старых файлов

--- Hard.py
+++ Hard.py
@@ -1,11 +1,4 @@
-N = int(input())
-count = 0
-
-for i in range(N):
-    a = input()
-    a = a.split()
-    for i in range(len(a)):
-        if a[i] == "зайка":
-            count += 1
-print(count)
-    
+a, b = input().split()
+a = int(a)
+b = int(b)
+print(a + b)
